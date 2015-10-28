1dd1f0459b830f6bd2e148e25216e72e1dd5b6f5
Ben Gohlke <jcgohlke@me.com>
2015-10-28 11:31:39 -0400

    Added kata4 to day18

---
+++ 18--Mary-Samsonite/kata4.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: ## Question 1
+//: ### Prime Numbers
+//: Implement the following functions. The divides function returns ```true``` if ```a``` is divisible by ```b``` and ```false``` otherwise. The ```countDivisors``` function should use the ```divides``` function to return the number of ```divisors``` of ```number```. The ```isPrimefunction``` should use the ```countDivisors``` function to determine if ```number``` is prime.
+
+func divides(a: Int, b: Int) -> Bool
+{
+}
+
+func countDivisors(number: Int) -> Int
+{
+}
+
+func isPrime(number: Int) -> Bool
+{
+}
+
+//: ## Question 2
+//: ### First Primes
+//: Using ```isPrime``` from the previous question, write a function named ```printFirstPrimes``` that takes a parameter named ```count``` of type ```Int``` that prints the first ```count``` prime numbers. For example, if ```count``` were set to 5, it would print "2, 3, 5, 7, 11"
+
+
+
+//: ## Question 3
+//: ### Reverse
+//: Write a function named ```reverse``` that takes an array of integers named ```numbers``` as a parameter. The function should return an array with the numbers from ```numbers``` in reverse order.
+
+
+
+//: ## Question 4
+//: ### Time Difference
+//: Write a function named ```timeDifference```. It takes as input four numbers that represent two times in a day and returns the difference in minutes between them. The first two parameters ```firstHour``` and ```firstMinute``` represent the hour and minute of the first time. The last two ```secondHour``` and ```secondMinute``` represent the hour and minute of the second time. All parameters should have external parameter names with the same name as the local ones.
+
+func timeDifference(firstHour: Int, firstMinute: Int, secondHour: Int, secondMinute: Int) -> Int
+{
+}
